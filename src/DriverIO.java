c54898c96841a9d28f74477c29f9a58ab3bb159b
rafaelgdgs <rafagsique@hotmail.com>
2023-09-28 08:16:04 -0300

    base DriveIO implementation

---
+++ src/DriverIO.java
@@ -0,0 +1,71 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DriverIO {
+
+    public static void writeToFile(WordTagFrequency wtf, Tags tags) {
+        List<String> ks = new ArrayList<>(tags.getTagsMap().keySet());
+        List<Integer> kv = new ArrayList<>(tags.getTagsMap().values());
+
+        List<Pair<String, Integer>> pairs = new ArrayList<>();
+
+        for (int i = 0; i < ks.size(); i++) {
+            pairs.add(new Pair<>(ks.get(i), kv.get(i)));
+        }
+        pairs.sort(Comparator.comparing(Pair::second));
+        for (int i = 0; i < pairs.size(); i++) {
+            ks.set(i, pairs.get(i).first());
+        }
+
+        try {
+            FileWriter myWriter = new FileWriter("out/PoS Output.txt");
+            myWriter.write("Tags " + ks + "\n");
+            for (String key: wtf.getFrequency().keySet()) {
+                myWriter.write(key + " " + Arrays.toString(wtf.getFrequency().get(key)) + "\n");
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred");
+            e.printStackTrace();
+        }
+    }
+
+    public static void readFromFile(String path) {
+        WordTagFrequency wtf = null;
+        try {
+            File myFile = new File(path);
+            Scanner myReader = new Scanner(myFile);
+
+            String tagsString = myReader.nextLine();
+            Tags tags = new Tags();
+
+            Pattern pattern = Pattern.compile(",\\s(\\w+)");
+            Matcher matcher = pattern.matcher(tagsString);
+
+            boolean primeiro = true;
+
+            while (matcher.find()) {
+                String tag = matcher.group();
+                if (!primeiro) {
+                    System.out.println(tag);
+                }
+                primeiro = false;
+            }
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+
+            }
+
+            myReader.close();
+        } catch (Exception e) {
+            System.out.println("An error occurred in the file reader.");
+            e.printStackTrace();
+        }
+        //return wtf;
+    }
+}
--- src/PoS.java
+++ src/PoS.java
@@ -1,7 +1,3 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.*;
-
 public class PoS {
 
     public static void main(String[] args) {
@@ -9,48 +5,25 @@ public class PoS {
         String path = "data/Secs0-18 - training";
 
         PoSData data = FileReader.readFromFile(path);
-        WordTagFrequency wtf = new WordTagFrequency(data);
+//        WordTagFrequency wtf = new WordTagFrequency(data);
 
-        System.out.println(Arrays.toString(data.getTagsArray()));
-        System.out.println(Arrays.toString(data.getTagsIndex()));
+//        System.out.println(Arrays.toString(data.getTagsArray()));
+//        System.out.println(Arrays.toString(data.getTagsIndex()));
 
-        List<String> ks = new ArrayList<>(data.getTags().getTagsMap().keySet());
-        List<Integer> kv = new ArrayList<>(data.getTags().getTagsMap().values());
+        DriverIO.readFromFile("out/PoS Output.txt");
 
-        List<Pair<String, Integer>> pairs = new ArrayList<>();
-
-        for (int i = 0; i < ks.size(); i++) {
-            pairs.add(new Pair<>(ks.get(i), kv.get(i)));
-        }
-        pairs.sort(Comparator.comparing(Pair::second));
-        for (int i = 0; i < pairs.size(); i++) {
-            ks.set(i, pairs.get(i).first());
-        }
-
-        try {
-            FileWriter myWriter = new FileWriter("out/PoS Output.txt");
-            myWriter.write("Tags " + ks + "\n");
-            for (String key: wtf.getFrequency().keySet()) {
-                myWriter.write(key + " " + Arrays.toString(wtf.getFrequency().get(key)) + "\n");
-            }
-            myWriter.close();
-        } catch (IOException e) {
-                System.out.println("An error occurred");
-                e.printStackTrace();
-        }
-
-        try (Scanner scanner = new Scanner(System.in)) {
-            String s;
-
-            try {
-                do {
-                    s = scanner.nextLine();
-                    System.out.println(wtf.probableTag(s));
-                } while (!s.isEmpty());
-            } catch(IllegalStateException | NoSuchElementException e) {
-                System.out.println("System.in was closed; exiting");
-            }
-        }
+//        try (Scanner scanner = new Scanner(System.in)) {
+//            String s;
+//
+//            try {
+//                do {
+//                    s = scanner.nextLine();
+//                    System.out.println(wtf.probableTag(s));
+//                } while (!s.isEmpty());
+//            } catch(IllegalStateException | NoSuchElementException e) {
+//                System.out.println("System.in was closed; exiting");
+//            }
+//        }
 
     }
 
