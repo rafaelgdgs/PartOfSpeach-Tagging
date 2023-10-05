5cff3de10a4aa1b64e0b16e9b758ddda17915b0c
rafaelgdgs <rafagsique@hotmail.com>
2023-10-05 10:13:39 -0300

    test the DriverIO.readFromFile method

--- src/PoS.java
+++ src/PoS.java
@@ -7,14 +7,16 @@ public class PoS {
         String path = "/home/jhin/workdata/Secs0-18 - training";
         String drivepath = "/home/jhin/workdata/PoS Output.txt";
 
-        PoSData data = FileReader.readFromFile(path);
-        WordTagFrequency wtf = new WordTagFrequency(data);
+        //PoSData data = FileReader.readFromFile(path);
+        //WordTagFrequency wtf = new WordTagFrequency();
+        //wtf.addPosData(data);
 
         //System.out.println(Arrays.toString(data.getTagsArray()));
         //System.out.println(Arrays.toString(data.getTagsIndex()));
 
-        DriverIO.writeToFile(wtf, drivepath);
-        DriverIO.readFromFile(drivepath);
+        //DriverIO.writeToFile(wtf, drivepath);
+        WordTagFrequency wtf = DriverIO.readFromFile(drivepath);
+        System.out.println(Arrays.toString(wtf.getTags().getTagsArray()));
 
         try (Scanner scanner = new Scanner(System.in)) {
             String s;
